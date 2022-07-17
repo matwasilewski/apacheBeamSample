5cd7ba6b50a168eb7840156fde81dd5c78782b5b
matwasilewski <mat.p.wasilewski@gmail.com>
2022-07-17 13:44:31 +0200

    move data to files and fixtures

--- tests/test_pipeline_2.py
+++ tests/test_pipeline_2.py
@@ -1,3 +1,5 @@
+import csv
+
 import apache_beam as beam
 import pytest
 from apache_beam.testing.test_pipeline import TestPipeline
@@ -6,14 +8,33 @@ from apache_beam.testing.util import assert_that, equal_to
 from apacheBeamSample.pipelines.pipeline_2 import FilterTransactions
 
 
-def test_filter_before_2010():
-    records = [
-        "2009-01-09 02:54:25 UTC,wallet00000e719adfeaa64b5a,wallet00001866cb7e0f09a890,100",
-        "2010-01-01 00:00:00 UTC,wallet00000e719adfeaa64b5a,wallet00001e494c12b3083634,200",
-        "2011-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,400",
-    ]
+@pytest.fixture
+def records_before_2010():
+    with open("resources/records_before_2010.csv") as f:
+        return f.readlines()
+
+
+@pytest.fixture
+def records_amount_greater_than_20():
+    with open("resources/records_amount_greater_than_20.csv") as f:
+        return f.readlines()
+
+
+@pytest.fixture
+def records_aggregate_by_date():
+    with open("resources/records_aggregate_by_date.csv") as f:
+        return f.readlines()
+
+
+@pytest.fixture
+def records_filter_and_aggregate():
+    with open("resources/records_filter_and_aggregate.csv") as f:
+        return f.readlines()
+
+
+def test_filter_before_2010(records_before_2010):
     with TestPipeline() as p:
-        records_pCollection = p | beam.Create(records)
+        records_pCollection = p | beam.Create(records_before_2010)
         output_pCollection = records_pCollection | FilterTransactions()
 
         assert_that(
@@ -27,18 +48,9 @@ def test_filter_before_2010():
         )
 
 
-def test_filter_for_amount_greater_than_20():
-    records = [
-        "2010-01-01 00:00:00 UTC,wallet00000e719adfeaa64b5a,wallet00001e494c12b3083634,5",
-        "2011-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,10",
-        "2012-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,19",
-        "2013-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,20",
-        "2014-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,21",
-        "2015-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,40",
-        "2016-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,100",
-    ]
+def test_filter_for_amount_greater_than_20(records_amount_greater_than_20):
     with TestPipeline() as p:
-        records_pCollection = p | beam.Create(records)
+        records_pCollection = p | beam.Create(records_amount_greater_than_20)
         output_pCollection = records_pCollection | FilterTransactions()
 
         assert_that(
@@ -53,22 +65,9 @@ def test_filter_for_amount_greater_than_20():
         )
 
 
-def test_aggregate_by_date():
-    records = [
-        "2011-01-01 10:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,25",
-        "2011-01-01 11:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,50",
-        "2012-01-01 12:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,100",
-        "2012-01-01 13:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,200",
-        "2012-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,400",
-        "2013-02-01 15:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,800",
-        "2013-02-01 16:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,1600",
-        "2013-03-01 17:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,3200",
-        "2013-03-01 18:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,6400",
-        "2013-03-02 19:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,12800",
-        "2013-03-02 20:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,25600",
-    ]
+def test_aggregate_by_date(records_aggregate_by_date):
     with TestPipeline() as p:
-        records_pCollection = p | beam.Create(records)
+        records_pCollection = p | beam.Create(records_aggregate_by_date)
         output_pCollection = records_pCollection | FilterTransactions()
 
         assert_that(
@@ -85,22 +84,9 @@ def test_aggregate_by_date():
         )
 
 
-def test_filter_and_aggregate():
-    records = [
-        "2008-01-01 10:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,25",
-        "2009-01-01 11:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,50",
-        "2010-01-01 12:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,100",
-        "2010-01-01 13:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,200",
-        "2012-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,400",
-        "2012-02-01 15:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,800",
-        "2012-02-01 16:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,1600",
-        "2015-03-01 17:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,3200",
-        "2015-03-01 18:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,6400",
-        "2003-03-02 19:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,12800",
-        "2002-03-02 20:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,25600",
-    ]
+def test_filter_and_aggregate(records_filter_and_aggregate):
     with TestPipeline() as p:
-        records_pCollection = p | beam.Create(records)
+        records_pCollection = p | beam.Create(records_filter_and_aggregate)
         output_pCollection = records_pCollection | FilterTransactions()
 
         assert_that(
