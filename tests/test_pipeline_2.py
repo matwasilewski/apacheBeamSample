b08273ead0e2c6c0a13b31881d22611b7aa3e533
matwasilewski <mat.p.wasilewski@gmail.com>
2022-07-17 12:59:45 +0200

    add test aggregate by date

--- tests/test_pipeline_2.py
+++ tests/test_pipeline_2.py
@@ -51,3 +51,35 @@ def test_filter_for_amount_greater_than_20():
                 ]
             ),
         )
+
+
+def test_aggregate_by_date():
+    records = [
+        "2011-01-01 10:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,25",
+        "2011-01-01 11:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,50",
+        "2012-01-01 12:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,100",
+        "2012-01-01 13:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,200",
+        "2012-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,400",
+        "2013-02-01 15:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,800",
+        "2013-02-01 16:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,1600",
+        "2013-03-01 17:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,3200",
+        "2013-03-01 18:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,6400",
+        "2013-03-02 19:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,12800",
+        "2013-03-02 20:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,25600",
+    ]
+    with TestPipeline() as p:
+        records_pCollection = p | beam.Create(records)
+        output_pCollection = records_pCollection | FilterTransactions()
+
+        assert_that(
+            output_pCollection,
+            equal_to(
+                [
+                    "2011-01-01,75.0",
+                    "2012-01-01,700.0",
+                    "2013-02-01,2400.0",
+                    "2013-03-01,9600.0",
+                    "2013-03-02,38400.0",
+                ]
+            ),
+        )
