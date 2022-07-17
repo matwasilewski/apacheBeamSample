d7e9b0846c9862d82812be69d7c65bb29533610f
matwasilewski <mat.p.wasilewski@gmail.com>
2022-07-17 12:42:27 +0200

    move parsing and additional functions to utils

--- src/apacheBeamSample/pipelines/pipeline_1.py
+++ src/apacheBeamSample/pipelines/pipeline_1.py
@@ -4,30 +4,7 @@ from datetime import datetime
 from apache_beam.options.pipeline_options import PipelineOptions
 import apache_beam as beam
 
-
-def parse_line(element):
-    for line in csv.DictReader(
-        [element],
-        fieldnames=[
-            "timestamp",
-            "origin",
-            "destination",
-            "transaction_amount",
-        ],
-    ):
-        formatted_line = {
-            "timestamp": datetime.strptime(
-                line["timestamp"], "%Y-%m-%d %H:%M:%S %Z"
-            ),
-            "origin": line["origin"],
-            "destination": line["destination"],
-            "transaction_amount": float(line["transaction_amount"]),
-        }
-        return formatted_line
-
-
-def to_strings(element):
-    return [str(v) for v in element.values()]
+from apacheBeamSample.utils import parse_line
 
 
 def run(options):
---
+++ src/apacheBeamSample/utils.py
@@ -0,0 +1,23 @@
+def parse_line(element):
+    for line in csv.DictReader(
+        [element],
+        fieldnames=[
+            "timestamp",
+            "origin",
+            "destination",
+            "transaction_amount",
+        ],
+    ):
+        formatted_line = {
+            "timestamp": datetime.strptime(
+                line["timestamp"], "%Y-%m-%d %H:%M:%S %Z"
+            ),
+            "origin": line["origin"],
+            "destination": line["destination"],
+            "transaction_amount": float(line["transaction_amount"]),
+        }
+        return formatted_line
+
+
+def to_strings(element):
+    return [str(v) for v in element.values()]
