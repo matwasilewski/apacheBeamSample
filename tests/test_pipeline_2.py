ca826c87c31b44092c80ef2d051bd81310ca3a3f
matwasilewski <mat.p.wasilewski@gmail.com>
2022-07-17 12:51:05 +0200

    add test case for filtering amounts

--- tests/test_pipeline_2.py
+++ tests/test_pipeline_2.py
@@ -25,3 +25,29 @@ def test_filter_before_2010():
                 ]
             ),
         )
+
+
+def test_filter_for_amount_greater_than_20():
+    records = [
+        "2010-01-01 00:00:00 UTC,wallet00000e719adfeaa64b5a,wallet00001e494c12b3083634,5",
+        "2011-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,10",
+        "2012-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,19",
+        "2013-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,20",
+        "2014-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,21",
+        "2015-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,40",
+        "2016-01-01 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,100",
+    ]
+    with TestPipeline() as p:
+        records_pCollection = p | beam.Create(records)
+        output_pCollection = records_pCollection | FilterTransactions()
+
+        assert_that(
+            output_pCollection,
+            equal_to(
+                [
+                    "2014-01-01,21.0",
+                    "2015-01-01,40.0",
+                    "2016-01-01,100.0",
+                ]
+            ),
+        )
